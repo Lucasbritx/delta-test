776bc65f510abc921f7ebe220c5ee72ddbc1e91c
Lucas Xavier <lucas.caldeira@gruporbs.com.br>
2023-08-20 11:27:26 -0300

    :sparkles: add favicon and ids in cards

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,39 +6,46 @@ import {
 } from "../../assets";
 import Banner from "../../components/Banner";
 import Card from "../../components/Card";
+import { v4 as uuidv4 } from "uuid";
 
 const cards = [
   {
+    id: uuidv4(),
     image: happyWoman,
     text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam",
     authorName: "Souza Costa",
     postDate: "18 de fevereiro de 2021",
   },
   {
+    id: uuidv4(),
     image: towTruck,
     text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam",
     authorName: "Juninho",
     postDate: "18 de fevereiro de 2021",
   },
   {
+    id: uuidv4(),
     image: mechanicTowTruck,
     text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam",
     authorName: "Nicolas",
     postDate: "18 de fevereiro de 2021",
   },
   {
+    id: uuidv4(),
     image: happyWoman2,
     text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam",
     postDate: "18 de fevereiro de 2021",
     authorName: "Jenifer",
   },
   {
+    id: uuidv4(),
     image: happyWoman,
     text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam",
     postDate: "18 de fevereiro de 2021",
     authorName: "Austin",
   },
   {
+    id: uuidv4(),
     image: towTruck,
     text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam",
     postDate: "18 de fevereiro de 2021",
@@ -53,6 +60,7 @@ export default function Home() {
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-14 gap-y-16">
         {cards.map((card) => (
           <Card
+            key={card.id}
             image={card.image}
             text={card.text}
             authorName={card.authorName}
