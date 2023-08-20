6def8643e50747f0f57bf66e11aeb6f29d5a620b
Lucas Xavier <lucas.caldeira@gruporbs.com.br>
2023-08-20 14:41:32 -0300

    :construction: testing bgColor as variable in tailwind

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -6,6 +6,7 @@ import user from "./user.svg";
 import banner1 from "./banner1.png";
 import banner2 from "./banner2.png";
 import banner3 from "./banner3.png";
+import deltaWhiteLogo from "./delta-white-logo.svg";
 
 export {
   happyWoman,
@@ -16,4 +17,5 @@ export {
   banner1,
   banner2,
   banner3,
+  deltaWhiteLogo,
 };
--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,19 +1,34 @@
 import React from "react";
 import Carousel from "../Carousel";
-import { banner1, banner2, banner3 } from "../../assets";
+import { banner1, banner2, banner3, deltaWhiteLogo } from "../../assets";
+
+const getSlide = (image: string, bgColor: string) => {
+  return (
+    <div className="relative">
+      <div className="absolute inset-0">
+        <div
+          className={`absolute rounded-xl inset-0 bg-[${bgColor}] opacity-95`}
+        >
+          <img
+            src={deltaWhiteLogo}
+            className="absolute right-0 opacity-10 h-full"
+          />
+        </div>
+      </div>
+      <img src={image} className="w-full h-full" />
+    </div>
+  );
+};
 
 const slides = [
-  <div className="relative">
-    <div className="absolute inset-0"></div>
-    <img src={banner1} className="w-full h-full" />
-  </div>,
-  <img src={banner2} className="w-full h-full" />,
-  <img src={banner3} className="w-full h-full" />,
+  getSlide(banner1, "#0272AAC2"),
+  getSlide(banner2, "#EDEFBEC2"),
+  getSlide(banner3, "#AA0202C2"),
 ];
 
 const Banner: React.FC = () => {
   return (
-    <div className="w-full ">
+    <div className="w-full">
       <Carousel slides={slides} />
     </div>
   );
