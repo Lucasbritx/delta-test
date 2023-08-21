b30286914494396b97e23277494e7a937b8f5ed7
Lucas Xavier <lucas.caldeira@gruporbs.com.br>
2023-08-21 14:00:35 -0300

    :lipstick: added lazy loading in cards

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { user } from "../../assets";
+import ContentLoader from "react-content-loader";
 
 interface ICard {
   image: string;
@@ -8,15 +9,31 @@ interface ICard {
   postDate: string;
 }
 
+const renderImgSkeleton = () => {
+  return (
+    <ContentLoader
+      viewBox="0 0 100% 100%"
+      height={"100%"}
+      width={"100%"}
+      className="w-full rounded-t-lg"
+      speed={1}
+    >
+      <rect x="0" y="0" rx="5" ry="5" width="100%" height="100%" />
+    </ContentLoader>
+  );
+};
+
 const Card: React.FC<ICard> = ({ image, text, authorName, postDate }) => {
+  const [isImageLoaded, setIsImageLoaded] = React.useState(false);
   return (
     <div className="w-full rounded-lg shadow-md flex flex-col max-w-sm sm:max-w-none">
+      {!isImageLoaded && renderImgSkeleton()}
       <img
         loading="lazy"
         src={image}
-        height="100%"
-        width="100%"
-        className="w-full rounded-t-lg object-cover"
+        onLoad={() => setIsImageLoaded(true)}
+        className={"w-full rounded-t-lg object-cover"}
+        style={{ height: isImageLoaded ? "100%" : "0" }}
         alt="Foto do card"
       />
       <div className="p-8 flex flex-col gap-y-4">
